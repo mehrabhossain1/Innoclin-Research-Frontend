612c9b38f690e1fa94cefd322dbdf86df98d62e3
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-08-26 21:40:29 +0600

    feat: services/grant plan & clinical study

---
+++ app/services/clinical-trial-optimization/page.tsx
@@ -0,0 +1,21 @@
+import ContactUsSection from "@/components/ContactUsSection";
+import PageHeading from "@/components/pages/PageHeading";
+import ClinicalStudyPageSection from "./ClinicalStudyPageSection";
+
+export default function ClinicalTrialPage() {
+    return (
+        <main className="flex flex-col">
+            {/* Hero Section */}
+            <PageHeading
+                title="Clinical Study Optimization"
+                subtitle=""
+                bgImage="https://img.freepik.com/premium-photo/digital-brain-concept-cosmic-background_98396-118211.jpg?w=1480"
+            />
+
+            {/* Intro Section */}
+
+            <ClinicalStudyPageSection />
+            <ContactUsSection />
+        </main>
+    );
+}
---
+++ app/services/grant-plan/page.tsx
@@ -0,0 +1,21 @@
+import ContactUsSection from "@/components/ContactUsSection";
+import PageHeading from "@/components/pages/PageHeading";
+import GrantPlanPageSection from "./GrantPlanPageSection";
+
+export default function ClinicalTrialPage() {
+    return (
+        <main className="flex flex-col">
+            {/* Hero Section */}
+            <PageHeading
+                title="Grant Planning & Funding Strategy"
+                subtitle=""
+                bgImage="https://img.freepik.com/premium-photo/digital-brain-concept-cosmic-background_98396-118211.jpg?w=1480"
+            />
+
+            {/* Intro Section */}
+
+            <GrantPlanPageSection />
+            <ContactUsSection />
+        </main>
+    );
+}
