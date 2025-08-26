1bb433c6e64018ad68ef461e2a9a0d7f5e4cbebe
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-08-26 19:49:12 +0600

    feat: services/consultancy/study-planning-&-monitoring

---
+++ app/services/consultancy/study-planning-&-monitoring/page.tsx
@@ -0,0 +1,21 @@
+import ContactUsSection from "@/components/ContactUsSection";
+import PageHeading from "@/components/pages/PageHeading";
+import StudyPlanAndMonitoringPageSection from "./StudyPlanAndMonitoringPageSection";
+
+export default function StudyPlanAndMonitoringPage() {
+    return (
+        <main className="flex flex-col">
+            {/* Hero Section */}
+            <PageHeading
+                title="Study Planning & Monitoring "
+                subtitle=""
+                bgImage="https://img.freepik.com/premium-photo/digital-brain-concept-cosmic-background_98396-118211.jpg?w=1480"
+            />
+
+            {/* Intro Section */}
+
+            <StudyPlanAndMonitoringPageSection />
+            <ContactUsSection />
+        </main>
+    );
+}
