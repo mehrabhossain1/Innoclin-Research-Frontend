9e0e3bbc27638e9a771e4d7e56fa0a9ff667f966
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-08-28 18:23:40 +0600

    improved: AboutUsSection

--- components/AboutUsSection.tsx
+++ components/AboutUsSection.tsx
@@ -4,15 +4,16 @@ import Image from "next/image";
 
 export default function AboutUsSection() {
     return (
-        <section className="py-40 bg-white">
-            <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+        <section className="py-32 bg-gradient-to-r from-gray-50 via-white to-gray-500">
+            <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-12 items-stretch">
                 {/* Left Side - Text */}
-                <div>
-                    <h2 className="text-3xl md:text-4xl font-normal mb-6 leading-snug">
+                <div className="flex flex-col ">
+                    <h2 className="text-3xl md:text-4xl font-bold mb-6 leading-snug text-gray-900">
                         Welcome to{" "}
                         <span className="text-primary">Innoclin Research</span>
                     </h2>
-                    <p className="text-gray-700 mb-4 leading-relaxed">
+
+                    <p className="text-gray-700 mb-5 leading-relaxed text-lg">
                         At{" "}
                         <span className="text-primary font-semibold">
                             Innoclin Research
@@ -27,20 +28,20 @@ export default function AboutUsSection() {
                         both patients and sponsors throughout the clinical
                         research journey.
                     </p>
-                    <p className="text-gray-700 leading-relaxed">
-                        With full of expertise, we are committed to advancing
-                        the impact of clinical research — improving lives,
+
+                    <p className="text-gray-700 leading-relaxed text-lg">
+                        With deep expertise, we are committed to advancing the
+                        impact of clinical research — improving lives,
                         accelerating breakthroughs, and ensuring every project
                         reaches its full potential. Through transparent
-                        communication, forward-thinking strategies,
-                        adaptability, and innovative solutions, we partner with
-                        our clients to achieve better outcomes and long-lasting
-                        success.
+                        communication, forward-thinking strategies, and
+                        innovative solutions, we partner with our clients to
+                        achieve better outcomes and long-lasting success.
                     </p>
                 </div>
 
                 {/* Right Side - Image */}
-                <div className="relative w-full h-[400px] md:h-[500px] rounded-lg overflow-hidden shadow-lg">
+                <div className="relative w-full h-[450px] md:h-[450px] rounded-2xl overflow-hidden shadow-xl">
                     <Image
                         src="https://img.freepik.com/free-photo/microbiologist-doctor-taking-blood-sample-tube-from-rack-with-machines-analysis-lab-background_482257-527.jpg?t=st=1755199313~exp=1755202913~hmac=162a0ecc42826d6715b46d7fc24f692a62d19fbd05910d498de9d0d925d5c227&w=1480"
                         alt="Innoclin Research Laboratory"
